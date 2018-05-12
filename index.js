0fc30a6fa27a8e6ca36f7bd2064f5ea18c410fd6
Maximilian Schorradt <schorradt@publicplan.de>
2018-05-12 13:28:05 +0200

    CORS & besseres Errorhandling

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.use(bodyParser({limit: '10mb', parameterLimit: 1000000000}));
 app.use(require('./routes/json'));
 app.use(require('./routes/mdb'));
--- routes/json/index.js
+++ routes/json/index.js
@@ -6,28 +6,34 @@ const upload = require('../../upload');
 
 router.post('/convert/json', upload.single('lupo'), (req, res) => {
   const mdbFile = mdb(req.file.path);
+  console.log(mdbFile);
   let result = {};
 
   mdbFile.tables((err, tables) => {
-    if (err)
-      throw err;
-
-    tables.forEach((table) => {
-      mdbFile.toCSV(table, (err, csv) => {
-        if (err)
-          throw err;
-
-        csvParse(csv, {columns: true}, (err, output) => {
-          if (err)
+    if (err) {
+      console.error(err);
+      res.sendStatus(500);
+    }
+    else {
+      tables.forEach((table) => {
+        mdbFile.toCSV(table, (err, csv) => {
+          if (err) {
             throw err;
-
-          result[table] = output;
-          if (Object.keys(result).length === tables.length) {
-            res.send(result);
           }
+
+          csvParse(csv, {columns: true}, (err, output) => {
+            if (err) {
+              throw err;
+            }
+
+            result[table] = output;
+            if (Object.keys(result).length === tables.length) {
+              res.send(result);
+            }
+          });
         });
       });
-    });
+    }
   });
 });
 
